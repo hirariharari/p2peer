5e571a271e026463841801ad2e144a41388eccd8
Pranav Kakaraparti <pkakaraparti@ufl.edu>
2021-02-18 09:18:08 -0500

    ParseConfig for Common.cfg

---
+++ Test.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Test {
+    public static void main(String[] args)
+    {
+        ParseConfig inst = new ParseConfig();
+    System.out.println(inst.get_number_of_preferred_neighbors());
+    }
+}
